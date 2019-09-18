b61035f2ae185736bfac96f2a988bb36d6f7890c
Anton Kroshilin <kroshilin.a@afkxxx.com>
2019-09-18 16:04:47 +0300

    modal window bug

---
+++ src/App.svelte
@@ -0,0 +1,22 @@
+<script>
+	export let name;
+	import 'bulma/css/bulma.css'
+	import { Input, Field, Button, Icon, Collapse, Modal } from 'svelma'
+  
+  let active = false
+</script>
+
+<style>
+	h1 {
+		color: purple;
+	}
+</style>
+
+<h1>Hello {name}!</h1>
+
+<Button class="block" on:click={() => active = !active}>Toggle</Button>
+<Modal bind:active={active}>
+  <Field label="Username" type="is-success" message="Username is available">
+    <Input id="username" name="username" type="text" placeholder="Text input" />
+</Field>
+</Modal>
